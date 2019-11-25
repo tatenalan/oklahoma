9b37ba4d101428cf010a1dfb2cf35ab8270fdcb6
Caminaur <jacaminaur@hotmail.com>
2019-11-25 11:19:38 -0300

    vista de home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('plantilla')
+@section('titulo')
+  Home Page
+@endsection
+@section('css')
+  <!-- css especifico del la vista -->
+@endsection
+@section('principal')
+  <!-- contenido especifico del body de la vista-->
+  ESTA ES NUESTRA PAGINA WEB
+  <a href="#">Comprar cerveza artesanal</a>
+  @endsection

3b8220fad3f623aef741392fdc84e9882b4d687a
taten <taten.usa@gmail.com>
2019-12-02 12:42:48 -0300

    migraciones parciales

--- database/migrations/2019_12_03_152431_create_products_table.php
+++ database/migrations/2019_12_03_152431_create_products_table.php
@@ -15,14 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('category');
+            $table->string('name', 50);
+            $table->string('category', 50);
             $table->integer('price');
             $table->boolean('onSale');
-            $table->integer('discount');
+            $table->integer('discount')->nullable();
+            $table->bigInteger('genre_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
             $table->foreign('genre_id')->references('id')->on('genres');
             $table->foreign('category_id')->references('id')->on('categories');
-            $table->foreign('image_id')->references('id')->on('images');
             $table->timestamps();
 
 

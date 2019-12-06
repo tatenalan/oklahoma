1c3c5ef591cb1364b534db0980e5f6fd66fca202
Caminaur <jacaminaur@hotmail.com>
2019-12-06 13:07:31 -0300

    cambio la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('first_name',50)->nullable();
+            $table->string('last_name',50)->nullable();
+            $table->string('email',100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('address',100);
+            $table->string('avatar',500);
             $table->timestamps();
+            // $table->unsignedBigInteger('cart_id')->nullable();
+            // $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
         });
     }
 

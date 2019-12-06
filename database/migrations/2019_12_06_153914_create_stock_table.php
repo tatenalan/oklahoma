e0743f5d3283b71eb32d7bd4485ac74407fac567
taten <taten.usa@gmail.com>
2019-12-06 12:50:02 -0300

    migracion de stock

---
+++ database/migrations/2019_12_06_153914_create_stock_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer("XS")->nullable();
+            $table->integer("S")->nullable();
+            $table->integer("M")->nullable();
+            $table->integer("L")->nullable();
+            $table->integer("XL")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}

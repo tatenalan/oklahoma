b789fe1079cca0bc738a55d39e1732178f2a178d
Caminaur <jacaminaur@hotmail.com>
2019-12-10 20:52:47 -0300

    cambios en las factories de los productos, para obtener numeros mas reales

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -9,17 +9,25 @@ use App\Category;
 
 $factory->define(Product::class, function (Faker $faker) {
     $genres = Genre::all();
+    $random = $faker->randomDigit();
     $categories = Category::all();
     $onSale = $faker->boolean(40);
     if ($onSale) {
-      $discount = $faker->numberBetween(10,80);
+      $discount = $faker->randomDigit()*5;
+      if ($discount==0) {
+        $discount = 15;
+      }
     }
     else {
       $discount = 0;
     }
+    if ($random==0) {
+      $random = 4;
+    }
+
     return [
       'name' => $faker->name,
-      'price' => $faker->numberBetween(1000,2500),
+      'price' => $random*$faker->numberBetween(2,7)*100,
       'onSale' => $onSale,
       'discount' => $discount,
       'genre_id' => $genres->random(),

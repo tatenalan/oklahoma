742c6b9fc2a1a1d5e0a9bcdb017aa4e6c5818c84
Caminaur <jacaminaur@hotmail.com>
2019-12-20 19:51:49 -0300

    carpeta de js

---
+++ public/js/product.js
@@ -0,0 +1,25 @@
+// Al clickear una miniatura cambia la imagen principal por la de la miniatura
+function changeImage(){
+  var imagenGrande = document.querySelector('.imagen-producto');
+  var fotosChicas = document.querySelectorAll(".imagen-producto-peque");
+  for (img of fotosChicas) {
+    addEventListener('click', function(event){
+        if(event.target.src != undefined)
+          imagenGrande.src = event.target.src
+    });
+  }
+  // Junto con unos cambios esteticos para indicar la seleccion
+
+  // Reset de los cambios esteticos
+  for (fotos of fotosChicas) {
+    fotos.style.border = 'none';
+    fotos.style.opacity = 1;
+  }
+  console.log(imagenGrande.src);
+  var imgs = fotosChicas;
+}
+function confirmar(){
+  var acepta = confirm('Esta seguro?');
+  if (!acepta){
+  }
+}

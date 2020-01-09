37716aca1a83cd239e48bdedafde2d319ace9161
Caminaur <jacaminaur@hotmail.com>
2020-01-09 00:12:02 -0300

    cambios en la vista de jean

--- resources/views/jeans.blade.php
+++ resources/views/jeans.blade.php
@@ -12,7 +12,11 @@ home
         @foreach ($products as $product)
         <div class="padding  col-6 col-md-4 col-lg-3">
           <div class="producto">
-            <a href="/product/{{$product->id}}"><img style="" class="img-productos" onmouseover="this.src='/storage/{{$product->images[1]->path}}',this.style='transform: scale(1.03)';" onmouseout="this.src='/storage/{{$product->images[0]->path}}',this.style='transform: scale(1)';" src="/storage/{{$product->images[0]->path}}" alt="{{$product->category->name}}"></a>
+            @if (isset($product->images[1]->path))
+              <a href="/product/{{$product->id}}"><img style="" class="img-productos" onmouseover="this.src='/storage/{{$product->images[1]->path}}',this.style='transform: scale(1.03)';" onmouseout="this.src='/storage/{{$product->images[0]->path}}',this.style='transform: scale(1)';" src="/storage/{{$product->images[0]->path}}" alt="{{$product->category->name}}"></a>
+            @else
+              <a href="/product/{{$product->id}}"><img style="" class="img-productos" src="/storage/{{$product->images[0]->path}}" alt="{{$product->category->name}}"></a>
+            @endif
             <h3>{{$product->category->name}}</h3>
             <p class="descripcion">{{$product->name}}</p>
             @if ($product->onSale==true && isset($product->discount))
